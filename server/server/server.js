cd421f558ffebb95184da8fddb1f5d30b58eccd7
jefersonjose69-code <jefersonjose69@gmail.com>
2026-09-22 20:22:36 -0300

    Update server.js

--- server/server/server.js
+++ server/server/server.js
@@ -6,13 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    sistema: "Shopee Produtos em Alta",
-    status: "online",
-    mensagem: "Servidor funcionando!"
-  });
-});
+// Mostra o painel do sistema
+app.use(express.static(__dirname));
 
 app.get("/api/status", (req, res) => {
   res.json({
