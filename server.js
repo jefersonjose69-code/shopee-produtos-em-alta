67f7e493b1532f6b004fbb7f7ae29ee92443d3b3
jefersonjose69-code <jefersonjose69@gmail.com>
2026-09-22 22:26:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,30 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
+const produtos = [
+  {
+    id: 1,
+    nome: "Organizador de Gavetas MDF",
+    preco: 51.90,
+    categoria: "Casa",
+    destaque: true
+  },
+  {
+    id: 2,
+    nome: "Fone Bluetooth",
+    preco: 39.90,
+    categoria: "Eletrônicos",
+    destaque: true
+  },
+  {
+    id: 3,
+    nome: "Suporte para Celular",
+    preco: 19.90,
+    categoria: "Acessórios",
+    destaque: true
+  }
+];
+
 app.get("/api/status", (req, res) => {
   res.json({
     sucesso: true,
@@ -18,7 +42,8 @@ app.get("/api/status", (req, res) => {
 app.get("/api/produtos", (req, res) => {
   res.json({
     sucesso: true,
-    produtos: []
+    total: produtos.length,
+    produtos: produtos
   });
 });
 
